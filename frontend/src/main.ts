233eb09a3f95e0a7740cd9153612ac62f179d502
ogu-kazemiya <135459827+ogu-kazemiya@users.noreply.github.com>
2025-06-20 22:21:03 +0900

    スタイルを仮置き

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 import { vuetify } from './plugins/vuetify'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
 app.use(router)
+app.use(pinia)
 app.use(vuetify)
 
 app.mount('#app')
--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -4,7 +4,6 @@ import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-
 export const vuetify = createVuetify({
   components,
   directives,
@@ -13,6 +12,24 @@ export const vuetify = createVuetify({
     aliases,
     sets: {
       mdi,
+    },
+  },
+  theme: {
+    defaultTheme: 'light',
+    themes: {
+      light: {
+        colors: {
+          primary: '#424242',
+          secondary: '#757575',
+          accent: '#EEEEEE',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FB8C00',
+          background: '#FFFFFF',
+          surface: '#FFFFFF',
+        },
+      },
     },
   },
 })
