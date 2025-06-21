0d008fe32ce66ac93b498d1bc15d9223654100a6
{hachimitsu} <{hachimitsu.38@icloud.com}>
2025-06-21 15:23:36 +0900

    Post Attendance

--- backend/service/attendance_impl.go
+++ backend/service/attendance_impl.go
@@ -2,12 +2,28 @@ package service
 
 import (
 	"github.com/eraxyso/go-template/repository"
+	"github.com/labstack/echo/v4"
+	openapi_types "github.com/oapi-codegen/runtime/types"
 )
 
 type AttendanceServiceImpl struct {
-	eventRepository repository.EventRepository
+	attendanceRepository repository.AttendanceRepository
 }
 
-func NewAttendanceServiceImpl(exampleRepository repository.EventRepository) *EventServiceImpl {
-	return &EventServiceImpl{}
+func NewAttendanceServiceImpl(exampleRepository repository.AttendanceRepository) *AttendanceServiceImpl {
+	return &AttendanceServiceImpl{}
+}
+
+func (es AttendanceServiceImpl) CancelAttendance(ctx echo.Context, eventID openapi_types.UUID) error{
+	if err := es.attendanceRepository.DeleteAttendance(ctx, eventID); err != nil {
+		return err
+	}
+	return nil	
+}
+
+func (es AttendanceServiceImpl) CreateEvent(ctx echo.Context, eventID openapi_types.UUID) error {
+	if err := es.attendanceRepository.InsertAttendance(ctx, eventID); err != nil {
+		return err
+	}
+	return nil
 }
