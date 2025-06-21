0742df9dc7d28d55b991dd9cf220769c205893e3
Eraxyso <130852025+Eraxyso@users.noreply.github.com>
2025-06-21 15:08:16 +0000

    feat: update wire.go and update db environment variable to fit neoshowcase

--- backend/wire.go
+++ backend/wire.go
@@ -14,18 +14,42 @@ import (
 )
 
 var (
-	exampleServiceBind = wire.Bind(
-		new(service.ExampleService),
-		new(*service.ExampleServiceImpl),
+	eventServiceBind = wire.Bind(
+		new(service.EventService),
+		new(*service.EventServiceImpl),
+	)
+	attendanceServiceBind = wire.Bind(
+		new(service.AttendanceService),
+		new(*service.AttendanceServiceImpl),
+	)
+	lotteryServiceBind = wire.Bind(
+		new(service.LotteryService),
+		new(*service.LotteryServiceImpl),
 	)
 	middlewareServiceBind = wire.Bind(
 		new(service.MiddlewareService),
 		new(*service.MiddlewareServiceImpl),
 	)
 
-	exampleRepositoryBind = wire.Bind(
-		new(repository.ExampleRepository),
-		new(*repository.ExampleRepositoryImpl),
+	adminRepositoryBind = wire.Bind(
+		new(repository.adminRepository),
+		new(*repository.adminRepositoryImpl),
+	)
+	attendeeRepositoryBind = wire.Bind(
+		new(repository.AttendeeRepository),
+		new(*repository.AttendeeRepositoryImpl),
+	)
+	eventRepositoryBind = wire.Bind(
+		new(repository.EventRepository),
+		new(*repository.EventRepositoryImpl),
+	)
+	lotteryRepositoryBind = wire.Bind(
+		new(repository.LotteryRepository),
+		new(*repository.LotteryRepositoryImpl),
+	)
+	winnerRepositoryBind = wire.Bind(
+		new(repository.WinnerRepository),
+		new(*repository.WinnerRepositoryImpl),
 	)
 )
 
@@ -33,13 +57,25 @@ func InitializeServer(db *gorm.DB) *handler.Handler {
 	wire.Build(
 		handler.NewHandler,
 
-		exampleServiceBind,
-		service.NewExampleServiceImpl,
+		eventServiceBind,
+		service.NewEventServiceImpl,
+		attendanceServiceBind,
+		service.NewAttendanceServiceImpl,
+		lotteryServiceBind,
+		service.NewLotteryServiceImpl,
 		middlewareServiceBind,
 		service.NewMiddlewareServiceImpl,
 
-		exampleRepositoryBind,
-		repository.NewExampleRepositoryImpl,
+		adminRepositoryBind,
+		repository.NewAdminRepositoryImpl,
+		attendeeRepositoryBind,
+		repository.NewAttendeeRepositoryImpl,
+		eventRepositoryBind,
+		repository.NewEventRepositoryImpl,
+		lotteryRepositoryBind,
+		repository.NewLotteryRepositoryImpl,
+		winnerRepositoryBind,
+		repository.NewWinnerRepositoryImpl,
 	)
 
 	return &handler.Handler{}
