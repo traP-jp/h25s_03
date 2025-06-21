397af7fd5c8b955122ebbf5c400dc1895c807539
{hachimitsu} <{hachimitsu.38@icloud.com}>
2025-06-21 14:27:57 +0900

    attendance

---
+++ backend/service/attendance.go
@@ -0,0 +1,10 @@
+package service
+
+import (
+	"github.com/labstack/echo/v4"
+	openapi_types "github.com/oapi-codegen/runtime/types"
+)
+
+type AttendanceService interface {
+	CancelAttendance(ctx echo.Context, eventID openapi_types.UUID) error
+}
---
+++ backend/service/attendance_impl.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+	"github.com/eraxyso/go-template/repository"
+)
+
+type AttendanceServiceImpl struct {
+	eventRepository repository.EventRepository
+}
+
+func NewAttendanceServiceImpl(exampleRepository repository.EventRepository) *EventServiceImpl {
+	return &EventServiceImpl{}
+}
