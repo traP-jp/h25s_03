8bd0c66455aa854e96026881681e03ffa959bca6
miyamon <212329262+miyamon222@users.noreply.github.com>
2025-06-22 10:56:39 +0900

    指摘された点の修正2

--- frontend/src/views/LotteryView.vue
+++ frontend/src/views/LotteryView.vue
@@ -3,11 +3,13 @@ import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { apiClient } from '@/api/apiClient'
 import type { components } from '@/api/schema'
+
 const ifDuplicated = ref(false)
 const route = useRoute()
 const eventId = route.params.eventId as string
 const lotteryId = route.params.lotteryId as string
 const lottery = ref<components['schemas']['Lottery'] | undefined>(undefined)
+
 const postLottery = async () => {
   await apiClient.POST('/events/{eventID}/lotteries/{lotteryID}', {
     params: {
@@ -17,7 +19,6 @@ const postLottery = async () => {
   })
   await fetchLottery()
 }
-
 const fetchLottery = async () => {
   const response = await apiClient.GET('/events/{eventID}/lotteries/{lotteryID}', {
     params: { path: { eventID: eventId, lotteryID: lotteryId } },
@@ -27,6 +28,7 @@ const fetchLottery = async () => {
 
 onMounted(fetchLottery)
 </script>
+
 <template>
   <v-container max-width="600">
     <v-card class="d-flex flex-column align-center ga-5">
@@ -35,7 +37,6 @@ onMounted(fetchLottery)
       </h1>
       <v-btn @click="postLottery"> 抽選スタート！！ </v-btn>
       <v-checkbox v-model="ifDuplicated" label="イベント内の重複当選を許可する"></v-checkbox>
-      <div class="d-flex"></div>
       <div v-if="lottery" class="d-flex ga-2">
         <span>当選者 :</span>
         <div>
