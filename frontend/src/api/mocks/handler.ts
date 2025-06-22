02e32dd6a81e60aa8efd670fe98f9c27df7ace59
cp-20 <n.naoki.1587@gmail.com>
2025-06-22 14:19:20 +0900

    feat: ルーレットの演出を作った

--- frontend/src/api/mocks/handler.ts
+++ frontend/src/api/mocks/handler.ts
@@ -7,13 +7,31 @@ export const handlers = [
   http.get('/events', () => {
     return HttpResponse.json([
       {
+        event_id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
         title: 'Event 1',
         description: 'Description for Event 1',
         date: '2023-10-01',
         is_open: true,
         is_me_admin: false,
+        admins: [],
+        is_me_attendee: false,
       },
     ])
+  }),
+  http.get('/events/{eventID}', () => {
+    return HttpResponse.json({
+      event_id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      title: '忘年会',
+      description: '楽しいイベント',
+      date: '2025-06-21T06:06:28.642Z',
+      is_open: true,
+      admins: ['miyamon', 'ogu_kazemiya'],
+      attendees: ['miyamon', 'ogu_kazemiya', 'ten_ten', 'cp20', 'Eraxyso', 'hachimitsu', 'Naph1'],
+      created_at: '2025-06-21T06:06:28.642Z',
+      updated_at: '2025-06-21T06:06:28.642Z',
+      is_deleted: false,
+      is_me_attendee: true,
+    })
   }),
   http.get('/events/{eventID}/lotteries/{lotteryID}', () => {
     return HttpResponse.json({
@@ -24,6 +42,11 @@ export const handlers = [
       created_at: '2025-06-21T06:06:28.642Z',
       updated_at: '2025-06-21T06:06:28.642Z',
       winners: ['miyamon', 'ogu_kazemiya', 'ten_ten'],
+    })
+  }),
+  http.post('/events/{eventID}/lotteries/{lotteryID}', () => {
+    return HttpResponse.json({
+      winner: 'miyamon',
     })
   }),
 ]
